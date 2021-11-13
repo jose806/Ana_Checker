6bd7b2ef0ef1a259596ee14db2dba885e262eec6
Jose Tolentino <47397412+jose806@users.noreply.github.com>
2021-11-13 15:43:49 -0600

    ignoreing config

--- src/config.js
+++ src/config.js
@@ -1,1 +1,12 @@
- export const BaseUrl = 'http://localhost:5000/attendee-tracker-308cc/us-central1/api'
+export const BaseUrl =
+  "http://localhost:5000/attendee-tracker-308cc/us-central1/api";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDo0sWDK9PIxvfbcCb6z_PO23ktq002SN8",
+  authDomain: "attendee-tracker-308cc.firebaseapp.com",
+  projectId: "attendee-tracker-308cc",
+  storageBucket: "attendee-tracker-308cc.appspot.com",
+  messagingSenderId: "299688955142",
+  appId: "1:299688955142:web:93dac3e9ff8bdc69572287",
+  measurementId: "G-3Y19EMGGMD",
+};
--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDo0sWDK9PIxvfbcCb6z_PO23ktq002SN8",
-  authDomain: "attendee-tracker-308cc.firebaseapp.com",
-  projectId: "attendee-tracker-308cc",
-  storageBucket: "attendee-tracker-308cc.appspot.com",
-  messagingSenderId: "299688955142",
-  appId: "1:299688955142:web:93dac3e9ff8bdc69572287",
-  measurementId: "G-3Y19EMGGMD",
-};
+import { firebaseConfig } from "./config";
 
 export const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 
 export const auth = getAuth(app);
